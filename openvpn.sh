527baa42ee30a879c69e4ece50562a602d2d99f8
fogiznt <85696249+fogiznt@users.noreply.github.com>
2021-10-24 09:59:27 +0500

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -15,7 +15,7 @@ echo -e "/ /_/ /  / _ \/ // / / _ \/ __// // /      / __/ / // / _ / // //_  _/"
 echo -e "\____/  /_.__/\_,_/ /_//_/\__/ \_,_/      /____/ \___/ (_)\___/  /_/  ";
 echo -e "                                                                      ${DEFAULT}";
 
-echo -n -e "${DEFAULT}Updating the package list  ${DEFAULT}" & echo -e ${GREEN} $(apt update 2>/dev/null | grep packages | cut -d '.' -f 1 | tr -cd '[[:digit:]]') "${DEFAULT}packages can be upgraded."
+echo -n -e "${DEFAULT}Updating the package list${DEFAULT}" & echo -e ${GREEN} $(apt update 2>/dev/null | grep packages | cut -d '.' -f 1 | tr -cd '[[:digit:]]') "${DEFAULT}packages can be upgraded."
 echo -e "Installing packages: "
 
 echo -n -e "               openvpn " & echo -n $(apt install openvpn -y >&- 2>&-)
